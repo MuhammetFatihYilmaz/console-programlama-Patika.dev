216d3fb71120c541990823cdf4f0d178c0899718
Muhammet Fatih Yılmaz <muhammet.yilmaz.1995@hotmail.com>
2022-01-08 21:12:39 +0300

    Added Switch-Case

--- Program.cs
+++ Program.cs
@@ -201,7 +201,8 @@ namespace console_programlama
             #endregion
 
             TryCatchFunc();
-            IfElseIf();
+            IfElseIfFunc();
+            SwitchCaseFunc();
 
 
         }
@@ -242,7 +243,7 @@ namespace console_programlama
             
         }
     
-        public static void IfElseIf()
+        public static void IfElseIfFunc()
         {
             int time = DateTime.Now.Hour;
 
@@ -257,10 +258,59 @@ namespace console_programlama
             Console.WriteLine(result);
 
             result = time>6 && time<11 ? "Günaydın." : time<18 ? "İyi günler." : "İyi akşamlar.";
-            Console.WriteLine(result);
+            Console.WriteLine(result);               
+        }
+
+        public static void SwitchCaseFunc()
+        {
+            int month = DateTime.Now.Month;
+
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak Ayındasınız.");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat Ayındasınız.");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart Ayındasınız.");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan Ayındasınız.");
+                    break;
                 
-                    
+                default:
+                break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış mevsimi");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar mevsimi");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz mevsimi");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar mevsimi");
+                    break;
                 
+                default:
+                break;
+            }
+
         }
     }
 }
