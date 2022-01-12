2e4f6cea4dfed7f007ed57e2bbe0c27e767c2d5b
Muhammet Fatih Yılmaz <muhammet.yilmaz.1995@hotmail.com>
2022-01-12 15:08:39 +0300

    Added DateTime and Math

--- Program.cs
+++ Program.cs
@@ -257,6 +257,7 @@ namespace console_programlama
             Console.WriteLine("**** Recursive ****");
 
             Console.WriteLine(_methodsCls.RecursiveFunc(3,4));
+            Console.WriteLine(_methodsCls.Factorial(3));
 
             Console.WriteLine("**** Extension ****");
             string name = "Patika .dev";
@@ -273,9 +274,8 @@ namespace console_programlama
 
             Console.WriteLine(isNumberEven.IsEven());
 
-            Console.WriteLine(_methodsCls.factorial(3));
-
             _methodsCls.StringMethod();
+            _methodsCls.DateTimeMethods();
 
 
 
@@ -637,11 +637,11 @@ namespace console_programlama
             //3*3*3*3 = 3^4
         }
 
-        public int factorial(int n)
+        public int Factorial(int n)
         {
             if(n==1)
             return 1;
-            return factorial(n-1)*n;
+            return Factorial(n-1)*n;
 
             //n==4
             //(4-1)*4 = 3*4 = 12
@@ -714,6 +714,70 @@ namespace console_programlama
             //SubString
             Console.WriteLine(sentence1.Substring(4));
             Console.WriteLine(sentence1.Substring(0,5));
+
+        }
+
+        public void DateTimeMethods()
+        {
+            Console.WriteLine("**** DateTime ****");
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now.Date);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+            Console.WriteLine(DateTime.Now.DayOfYear);
+
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+
+            Console.WriteLine(DateTime.Now.AddDays(5));
+            Console.WriteLine(DateTime.Now.AddMonths(2));
+            Console.WriteLine(DateTime.Now.AddYears(8));
+            Console.WriteLine(DateTime.Now.AddHours(3));
+            Console.WriteLine(DateTime.Now.AddMinutes(7));
+            Console.WriteLine(DateTime.Now.AddSeconds(2));
+            Console.WriteLine(DateTime.Now.AddMilliseconds(40));
+
+
+            Console.WriteLine("**** DateTime Format ****");
+            Console.WriteLine(DateTime.Now.ToString("dd"));   //15
+            Console.WriteLine(DateTime.Now.ToString("ddd"));  //Sat 
+            Console.WriteLine(DateTime.Now.ToString("dddd")); //Saturday
+
+            Console.WriteLine(DateTime.Now.ToString("MM"));   //04
+            Console.WriteLine(DateTime.Now.ToString("MMM"));  //Apr
+            Console.WriteLine(DateTime.Now.ToString("MMMM")); //April
+
+            Console.WriteLine(DateTime.Now.ToString("yy"));   //21
+            Console.WriteLine(DateTime.Now.ToString("yyyy")); //2021
+
+
+            Console.WriteLine("**** Math ****");
+            Console.WriteLine(Math.Abs(-5));
+            Console.WriteLine(Math.Sin(10));
+            Console.WriteLine(Math.Cos(10));
+            Console.WriteLine(Math.Tan(10));
+
+            Console.WriteLine(Math.Ceiling(2.3));
+            Console.WriteLine(Math.Round(2.7));
+            Console.WriteLine(Math.Round(2.3));
+            Console.WriteLine(Math.Floor(2.7));
+
+            Console.WriteLine(Math.Min(2,6));
+            Console.WriteLine(Math.Max(2,6));
+
+            Console.WriteLine(Math.Pow(3,4));
+            Console.WriteLine(Math.Sqrt(9));
+            Console.WriteLine(Math.Log(3));
+            Console.WriteLine(Math.Exp(3));
+            Console.WriteLine(Math.Log10(10));
 
 
 
